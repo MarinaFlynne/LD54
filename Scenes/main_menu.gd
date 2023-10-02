c8fc632aeb194054883fa7ba6088981e956af58e
Marina <111331059+MarinaFlynne@users.noreply.github.com>
2023-10-01 17:53:34 -0700

    add title screen

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var cloud_speed = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,8 +9,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	move_clouds(delta)
+	
 
+func move_clouds(delta):
+	var cloud1_pos = $Clouds.position.x
+	var cloud2_pos = $Clouds2.position.x
+	
+	if cloud2_pos >= 2304:
+		$Clouds2.position.x = -2301
+	if cloud1_pos >= 2304:
+		$Clouds.position.x = -2301
+	$Clouds.position.x += cloud_speed * delta
+	$Clouds2.position.x += cloud_speed * delta
 
 func _on_play_pressed():
+	AudioManager.play("res://SFX/titlestart.wav", -8)
+	print("play")
 	SceneManager.SwitchScene("fishing")
+
+
+func _on_blink_timer_timeout():
+	if $"Press Space".visible:
+		$"Press Space".hide()
+	else:
+		$"Press Space".show()
